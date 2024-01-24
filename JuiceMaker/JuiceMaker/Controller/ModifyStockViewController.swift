5136ec4fbdc560e4e6a1bf2ba730addda0f866ff
Diana-yjh <projin1234@gmail.com>
2024-01-24 15:31:47 +0900

    feat: ViewController에서 ModifyStockViewController로 화면전환 코드 추가

---
+++ JuiceMaker/JuiceMaker/Controller/ModifyStockViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ModifyStockViewController.swift
+//  JuiceMaker
+//
+//  Created by Yejin Hong on 2024/01/24.
+//
+
+import UIKit
+
+class ModifyStockViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- JuiceMaker/JuiceMaker/Controller/ViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/ViewController.swift
@@ -7,7 +7,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var modifyStockButton: UIBarButtonItem!
+    
     var juiceMaker = JuiceMaker()
+    
+    @IBAction func modifyStockButtonTapped(_ sender: Any) {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "ModifyStockViewController") as? ModifyStockViewController else {
+            return
+        }
+        vc.modalPresentationStyle = .currentContext
+        self.present(vc, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
